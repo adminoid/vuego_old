af317f807e7d8e2509b6a6f02596b251f7fdc93f
Petr Zenin <mr@adminoid.com>
2021-04-27 16:53:24 +0300

    test config with module

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,9 @@
 package main
 
-//import "github.com/adminoid/vuego/internal/app"
+import "vuego/internal/app"
 
 const configPath = "configs/main"
 
 func main() {
-	//app.run(configPath)
+	app.run(configPath)
 }
--- internal/app/main.go
+++ internal/app/main.go
@@ -1,1 +1,9 @@
 package app
+
+import (
+	"fmt"
+)
+
+func run(config) {
+	fmt.Println(config)
+}
