543c53ab36960a3a7c1e819299a6a4a640abc7d8
Petr Zenin <mr@adminoid.com>
2021-06-05 18:33:16 +0300

    replaced yaml to dotenv

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/adminoid/vuego/internal/app"
+	"github.com/joho/godotenv"
+	"log"
 )
 
-const configPath = "configs/main"
-
 func main() {
-	app.Run(configPath)
+	var myEnv map[string]string
+	myEnv, err := godotenv.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbUrl := myEnv["DATABASE"]
+	app.Run(dbUrl)
 }
